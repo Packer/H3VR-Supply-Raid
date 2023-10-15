5f780645e6063b6b88d7a053da705f2f42d267b3
Packer <glennbermingham@gmail.com>
2023-10-15 20:01:33 +1100

    1.0.2 Release

---
+++ Supply Raid Editor/Assets/Scripts/FactionUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Supply_Raid_Editor
+{
+    public class FactionUI : MonoBehaviour
+    {
+        public void TrySaveFaction()
+        {
+            
+        }
+    }
+}
